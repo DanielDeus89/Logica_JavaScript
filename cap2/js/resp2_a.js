8a662cab0763395a3bca2fa2f58964d0fd0c7862
DanielDeus <danielsilvadeus@gmail.com>
2023-02-02 17:27:33 -0300

    Finalizado Lista de Exemplos

---
+++ cap2/js/resp2_a.js
@@ -0,0 +1,7 @@
+// a) Umafarmáciaestácom uma promoção – Na compra de duas unidades de um
+// mesmo medicamento, o cliente recebe como desconto os centavos do
+// valor total. Elaborar um programa que leia descrição e preço de um
+// medicamento. Informe o valor do produto na promoção. A Figura 2.11
+// apresenta a tela com um exemplo de dados de entrada e saída do
+// programa.
+

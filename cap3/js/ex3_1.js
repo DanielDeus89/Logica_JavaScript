c38eee157472fb9dc3a808b10893761e42e0910e
DanielDeus <danielsilvadeus@gmail.com>
2023-02-03 15:20:42 -0300

    Exemplos 3.1 Livro de Logica JS

--- cap3/js/ex3_1.js
+++ cap3/js/ex3_1.js
@@ -3,21 +3,30 @@ function calcularMedia(){
     let inNota1 = document.getElementById("inNota1");    
     let inNota2 = document.getElementById("inNota2");    
 
-    let outSituacao = document.getElementById("outSituacao");
-    let outMedia = document.getElementById("outMedia");
-
     let aluno = inAluno.value;
     let nota1 = Number(inNota1.value);
     let nota2 = Number(inNota2.value);
 
     let media = (nota1+nota2)/2;
 
+    let situacao, corStatus;
     if(media>=7){
-        outSituacao.textContent = `Parabens ${aluno}! Você foi aprovado(a)`;
-        outSituacao.style.color = `blue`;
+        situacao = `Parabens ${aluno}! Você foi aprovado(a)`;
+        corStatus = `blue`;
+    }else if(media>=4){
+        situacao = `Atenção ${aluno}! Você está em exame`;
+        corStatus = `green`;
+    }else{
+        situacao = `Ops ${aluno}! Você foi reprovado(a)`;
+        corStatus = `red`;
     }
 
-    document.getElementById("outMedia").textContent = `Média das Notas:  ${media.toFixed(1)}`;
+    let outMedia = document.getElementById("outMedia");
+    outMedia.textContent = `Média das Notas:  ${media.toFixed(1)}`;
+
+    let outSituacao = document.getElementById("outSituacao");
+    outSituacao.textContent = situacao;
+    outSituacao.style.color = corStatus;
     
 }
 let exibirMedia = document.getElementById("exibirMedia").addEventListener("click", calcularMedia);

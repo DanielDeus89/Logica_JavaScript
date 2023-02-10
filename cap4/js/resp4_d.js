d8358bd6b824320c557ce5134bbb53f73536407e
DanielDeus <danielsilvadeus@gmail.com>
2023-02-09 23:10:00 -0300

    cap4/js/resp4_d.js

--- cap4/js/resp4_d.js
+++ cap4/js/resp4_d.js
@@ -3,4 +3,24 @@
 // incrementado. Observação: caso você informe um valor alto, as linhas
 // podem ultrapassar o tamanho da imagem e se alinhar à margem esquerda
 // da página. Para evitar que isso ocorra, crie uma nova classe no arquivo
-// estilos.css, que de
+// estilos.css, 
+
+function criarEstrelas(){
+    let inNumeroLinhas = document.getElementById("inNumeroLinhas");
+
+    let numeroLinhas = Number(inNumeroLinhas.value);
+
+    if(!Number.isInteger(numeroLinhas )|| numeroLinhas <= 0){
+        document.getElementById("outEstrelas").textContent = "Por favor, insira um número inteiro positivo.";
+        inNumeroLinhas.focus();
+        return;
+    }
+
+    let resp = "";
+    for(let i=0; i<= numeroLinhas; i++){
+        resp += `${'*'.repeat(i)}\n`;
+    }
+
+    document.getElementById("outEstrelas").textContent = resp;
+}
+let btCriarEstrelas = document.getElementById("btCriarEstrelas").addEventListener("click", criarEstrelas);

4bbf804e3a336a458d702fb01168f191836d10ad
DanielDeus <danielsilvadeus@gmail.com>
2023-02-09 22:41:31 -0300

    cap4/js/resp4_c.js

--- cap4/js/resp4_c.js
+++ cap4/js/resp4_c.js
@@ -3,3 +3,34 @@
 // divisores inteiros (exceto o próprio número). O programa deve exibir os
 // divisores do número e a soma deles. A Figura 4.17 exibe a página do
 // programa com um exemplo de número perfeito
+
+function verificarPerfeito(){
+    let inNumero = document.getElementById("inNumero");
+
+    let numero = Number (inNumero.value);
+
+    if(isNaN(numero) || numero <= 0){
+        alert("Insira um valor valido");
+        return;
+    }
+
+    let divisores = "";
+    let soma = 0;
+    for(let i=1; i<=numero/2; i++){
+        if(numero % i == 0){
+            divisores += `${i}, `;
+            soma += i;
+        }       
+    }
+
+    let msg = "";
+    if(soma === numero){
+        msg = `${numero} É um Número Perfeito`;
+    }else{
+        msg = `${numero} Não é um Número Perfeito`;
+
+    }
+    document.getElementById("outDivisores").textContent = `Divisores de ${numero}: ${divisores} (Soma: ${soma})`;
+    document.getElementById("outResultado").textContent = `${msg}`;
+}
+let btVerificar = document.getElementById("btVerificar").addEventListener("click",verificarPerfeito);

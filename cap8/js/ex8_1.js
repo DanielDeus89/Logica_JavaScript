71479ead3f9dc3c4be71bfda67b20bf0a80ca160
DanielDeus <danielsilvadeus@gmail.com>
2023-02-12 20:17:48 -0300

    modified:   cap8/js/ex8_1.js

--- cap8/js/ex8_1.js
+++ cap8/js/ex8_1.js
@@ -1,48 +1,46 @@
-function trocarCluber(){
+function trocarClube(){
     let imgClube = document.getElementById("imgClube")
-    let divTitulo = document.getElementById("divTitulo")
+    let divTitullo = document.getElementById("divTitullo")
 
-    let clube
-
+    let clube;
+    
     if(rbBrasil.checked){
-        clube = "Brasil"
+        clube = `Brasil`
     }else if(rbPelotas.checked){
-        clube = "Pelotas"
+        clube = `Pelotas`
     }else{
-        clube = "Farroupilha"
+        clube = `Farroupilha`
     }
 
-    divTitulo.className = "row cores"+clube
+    divTitullo.className = `row cores`+clube
 
-    imgClube.src = `./img/${clube.toLocaleLowerCase()}.png`
-    imgClube.className = "exibe"
+    imgClube.src = `./img/${clube.toLowerCase()}.png`
+    imgClube.className = `exibe`
     imgClube.alt = `Simbolo do ${clube}`
 
-    localStorage.setItem("clube", clube)
+    localStorage.setItem((`clube`, clube))
 }
 
 let rbBrasil = document.getElementById("rbBrasil")
 let rbPelotas = document.getElementById("rbPelotas")
 let rbFarroupilha = document.getElementById("rbFarroupilha")
 
-rbBrasil.addEventListener("change", trocarCluber)
-rbPelotas.addEventListener("change", trocarCluber)
-rbFarroupilha.addEventListener("change", trocarCluber)
+rbBrasil.addEventListener("change", trocarClube)
+rbPelotas.addEventListener("change", trocarClube)
+rbFarroupilha.addEventListener("change", trocarClube)
 
 function verificarClube(){
-    if(localStorage.getItem("clube")){
-        let clube = localStorage.getItem("clube")
+    if(localStorage.getItem(`clube`)){
+        let clube = localStorage.getItem(`clube`)
 
         if(clube == "Brasil"){
-            rbBrasil.checked = true
+            rbBrasil.checked = true;
         }else if(clube == "Pelotas"){
             rbPelotas.checked = true
         }else{
             rbFarroupilha.checked = true
         }
-
-        trocarCluber()
+        trocarClube()
     }
 }
-
 verificarClube()
